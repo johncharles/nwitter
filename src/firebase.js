e874e33fb58387f879bfd662401645ef5c927efd
Charles <jeochu@gmail.com>
2020-10-09 12:54:09 +0900

    api key 0.2

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,11 @@
 import * as firebase from "firebase/app"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA0qqylPqSUVI5d7d_8dBHh0JYrEX79--I",
-    authDomain: "nwitter-2d24d.firebaseapp.com",
-    databaseURL: "https://nwitter-2d24d.firebaseio.com",
-    projectId: "nwitter-2d24d",
-    storageBucket: "nwitter-2d24d.appspot.com",
-    messagingSenderId: "386726893142",
-    appId: "1:386726893142:web:7b97504c0593759207cdf7"
+    apiKey: process.env.REACT_APP_API_KEY,                        //"AIzaSyA0qqylPqSUVI5d7d_8dBHh0JYrEX79--I",
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,          //"nwitter-2d24d.firebaseapp.com",
+    databaseURL: process.env.REACT_APP_DATABASE_URL,      //"https://nwitter-2d24d.firebaseio.com",
+    projectId: process.env.REACT_APP_PROJECT_ID,            //"nwitter-2d24d",
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,   //"nwitter-2d24d.appspot.com",
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,   //"386726893142",
+    appId: process.env.REACT_APP_APP_ID                           //"1:386726893142:web:7b97504c0593759207cdf7"
   };
-
-  // Initialize Firebase
-  export default firebase.initializeApp(firebaseConfig);
